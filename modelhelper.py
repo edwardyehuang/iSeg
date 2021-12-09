f71e309d84a6a1513436701f050088f6597d7e65
edwardyehuang <edward.ye.huang@qq.com>
2021-12-09 17:26:28 +0800

    common mode setup moved to iseg

--- modelhelper.py
+++ modelhelper.py
@@ -5,6 +5,42 @@
 
 import tensorflow as tf
 
+from iseg.utils.keras_ops import set_bn_epsilon, set_bn_momentum, set_weight_decay
+
+def model_common_setup (model, restore_checkpoint = True,
+                               checkpoint_dir = None,
+                               max_checkpoints_to_keep = 1,
+                               weight_decay = None,
+                               bn_epsilon = None,
+                               bn_momentum = None,
+                               backbone_bn_momentum = None,
+                               inference_sliding_window_size = None):
+
+    model.inference_sliding_window_size = inference_sliding_window_size
+
+    model_helper = ModelHelper(model, checkpoint_dir, max_checkpoints_to_keep)
+
+    if restore_checkpoint:
+        model_helper.restore_checkpoint()
+
+    if weight_decay is not None:
+        set_weight_decay(model_helper.model, weight_decay)
+
+    if bn_epsilon is not None:
+        set_bn_epsilon(model_helper.model, bn_epsilon)
+
+    if bn_momentum is not None:
+        set_bn_momentum(model_helper.model, bn_momentum)
+
+    if backbone_bn_momentum is not None and hasattr(model_helper.model, "backbone"):
+        set_bn_momentum(model_helper.model.backbone, backbone_bn_momentum)
+    
+    # frezze_batch_norms(model_helper.model, FLAGS.bn_freeze)
+    
+    return model_helper
+
+
+
 class ModelHelper:
 
     def __init__ (self, model : tf.keras.Model, checkpoint_dir, max_to_keep = 20):
