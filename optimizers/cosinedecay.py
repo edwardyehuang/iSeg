50eff20dde0db49bf191e1a218cc4a9c096332a7
edwardyehuang <edward.ye.huang@qq.com>
2021-12-09 16:45:51 +0800

    structure improvements

---
+++ optimizers/cosinedecay.py
@@ -0,0 +1,82 @@
+# Copied from TensorFlow Addon repo
+
+import math
+import tensorflow as tf
+
+class CosineDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    """
+    A LearningRateSchedule that uses a cosine decay schedule.
+    See [Loshchilov & Hutter, ICLR2016](https://arxiv.org/abs/1608.03983),
+    SGDR: Stochastic Gradient Descent with Warm Restarts.
+    When training a model, it is often useful to lower the learning rate as
+    the training progresses. This schedule applies a cosine decay function
+    to an optimizer step, given a provided initial learning rate.
+    It requires a `step` value to compute the decayed learning rate. You can
+    just pass a TensorFlow variable that you increment at each training step.
+    The schedule a 1-arg callable that produces a decayed learning
+    rate when passed the current optimizer step. This can be useful for changing
+    the learning rate value across different invocations of optimizer functions.
+    It is computed as:
+    ```python
+    def decayed_learning_rate(step):
+        step = min(step, decay_steps)
+        cosine_decay = 0.5 * (1 + cos(pi * step / decay_steps))
+        decayed = (1 - alpha) * cosine_decay + alpha
+        return initial_learning_rate * decayed
+    ```
+    Example usage:
+    ```python
+    decay_steps = 1000
+    lr_decayed_fn = tf.keras.optimizers.schedules.CosineDecay(
+        initial_learning_rate, decay_steps)
+    ```
+    You can pass this schedule directly into a `tf.keras.optimizers.Optimizer`
+    as the learning rate. The learning rate schedule is also serializable and
+    deserializable using `tf.keras.optimizers.schedules.serialize` and
+    `tf.keras.optimizers.schedules.deserialize`.
+    Returns:
+        A 1-arg callable learning rate schedule that takes the current optimizer
+        step and outputs the decayed learning rate, a scalar `Tensor` of the same
+        type as `initial_learning_rate`.
+    """
+
+    def __init__(self, initial_learning_rate, decay_steps, alpha=0.0, name=None):
+        """Applies cosine decay to the learning rate.
+        Args:
+        initial_learning_rate: A scalar `float32` or `float64` Tensor or a
+            Python number. The initial learning rate.
+        decay_steps: A scalar `int32` or `int64` `Tensor` or a Python number.
+            Number of steps to decay over.
+        alpha: A scalar `float32` or `float64` Tensor or a Python number.
+            Minimum learning rate value as a fraction of initial_learning_rate.
+        name: String. Optional name of the operation.  Defaults to 'CosineDecay'.
+        """
+        super(CosineDecay, self).__init__()
+
+        self.initial_learning_rate = initial_learning_rate
+        self.decay_steps = decay_steps
+        self.alpha = alpha
+        self.name = name
+
+    def __call__(self, step):
+        with tf.name_scope(self.name or "CosineDecay"):
+            initial_learning_rate = tf.convert_to_tensor(self.initial_learning_rate, name="initial_learning_rate")
+            dtype = initial_learning_rate.dtype
+            decay_steps = tf.cast(self.decay_steps, dtype)
+
+            global_step_recomp = tf.cast(step, dtype)
+            global_step_recomp = tf.math.minimum(global_step_recomp, decay_steps)
+            completed_fraction = global_step_recomp / decay_steps
+            cosine_decayed = 0.5 * (1.0 + tf.math.cos(tf.constant(math.pi) * completed_fraction))
+
+            decayed = (1 - self.alpha) * cosine_decayed + self.alpha
+            return tf.math.multiply(initial_learning_rate, decayed)
+
+    def get_config(self):
+        return {
+            "initial_learning_rate": self.initial_learning_rate,
+            "decay_steps": self.decay_steps,
+            "alpha": self.alpha,
+            "name": self.name
+        }
---
+++ optimizers/polydecay.py
@@ -0,0 +1,74 @@
+# ================================================================
+# MIT License
+# Copyright (c) 2021 edwardyehuang (https://github.com/edwardyehuang)
+# ================================================================
+
+import tensorflow as tf
+
+class WarmUpPolyDecay (tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    def __init__(self, initial_learning_rate, decay_steps, end_learning_rate = 0.0001, warmup_steps = 0, warmup_learning_rate = 1e-4, power = 1.0, name = None):
+        """Applies a warmup polynomial decay to the learning rate.
+        Args:
+        initial_learning_rate: A scalar `float32` or `float64` `Tensor` or a
+            Python number.  The initial learning rate.
+        decay_steps: A scalar `int32` or `int64` `Tensor` or a Python number.
+            Must be positive.  See the decay computation above.
+        end_learning_rate: A scalar `float32` or `float64` `Tensor` or a
+            Python number.  The minimal end learning rate.
+        power: A scalar `float32` or `float64` `Tensor` or a
+            Python number.  The power of the polynomial. Defaults to linear, 1.0.
+        cycle: A boolean, whether or not it should cycle beyond decay_steps.
+        name: String.  Optional name of the operation. Defaults to
+            'PolynomialDecay'.
+        """
+        super(WarmUpPolyDecay, self).__init__()
+
+        self.initial_learning_rate = initial_learning_rate
+        self.decay_steps = decay_steps
+        self.end_learning_rate = end_learning_rate
+        self.power = power
+        self.warmup_steps = warmup_steps
+        self.warmup_learning_rate = warmup_learning_rate
+        self.name = name
+
+
+    
+    def __call__(self, step):
+        with tf.name_scope(self.name or "WarmUpPolyDecay") as name:
+            initial_learning_rate = tf.convert_to_tensor(self.initial_learning_rate, name="initial_learning_rate")
+            dtype = initial_learning_rate.dtype
+            end_learning_rate = tf.cast(self.end_learning_rate, dtype)
+            power = tf.cast(self.power, dtype)
+
+            current_step = tf.cast(step, dtype)
+            max_steps = tf.cast(self.decay_steps, dtype)
+         
+            current_step = tf.math.minimum(current_step, max_steps)
+
+            adjusted_slow_start_learning_rate = self.warmup_learning_rate
+            adjusted_current_step = current_step
+
+            if self.warmup_steps > 0:
+                adjusted_current_step = tf.maximum(adjusted_current_step - self.warmup_steps, 0)
+                adjusted_slow_start_learning_rate = (self.warmup_learning_rate + 
+                                                     (initial_learning_rate - self.warmup_learning_rate) * 
+                                                     tf.cast(current_step, tf.float32) / 
+                                                     self.warmup_steps)
+
+            p = tf.math.divide(adjusted_current_step, max_steps)
+
+            learning_rate = tf.add(tf.math.multiply(initial_learning_rate - end_learning_rate, tf.math.pow(1 - p, power)), end_learning_rate, name = name)
+
+            return tf.cond(step < self.warmup_steps, lambda: adjusted_slow_start_learning_rate, lambda: learning_rate)
+
+    def get_config(self):
+        return {
+            "initial_learning_rate": self.initial_learning_rate,
+            "decay_steps": self.decay_steps,
+            "end_learning_rate": self.end_learning_rate,
+            "power": self.power,
+            "warmup_steps":self.warmup_steps,
+            "warmup_learning_rate":self.warmup_learning_rate,
+            "name": self.name
+        }
