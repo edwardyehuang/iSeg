4221f57027628ec582afa610ed29387f52e84396
edwardyehuang <edward.ye.huang@qq.com>
2023-01-20 08:02:34 +0800

    Make some inner code public

---
+++ layers/se.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+from iseg.layers.normalizations import normalization
+
+
+class SqueezeAndExcitationModule(tf.keras.Model):
+    def __init__(self, ratio=16, name=None):
+        super().__init__(name=name)
+
+        self.ratio = ratio
+
+    def build(self, input_shape):
+
+        filters = input_shape[-1]
+
+        self.down_conv = tf.keras.layers.Conv2D(int(filters / self.ratio), (1, 1), use_bias=True, name="down_conv")
+        self.expand_conv = tf.keras.layers.Conv2D(filters, (1, 1), use_bias=True, name="expand_conv")
+        
+
+    def call(self, inputs, training=None):
+
+        x = inputs
+
+        x = tf.math.reduce_mean(x, axis=[1, 2], keepdims=True)
+        x = self.down_conv(x)
+        x = tf.nn.relu(x)
+
+        x = self.expand_conv(x)
+        x = tf.nn.sigmoid(x)
+        x = tf.cast(x, inputs.dtype)
+
+        return x * inputs
