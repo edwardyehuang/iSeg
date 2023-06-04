abc640a054299f86d0b4bad36e9904f4a9fb8400
edwardyehuang <edward.ye.huang@qq.com>
2023-06-04 22:51:56 +0800

    convert to lower

--- backbones/backbone_registry.py
+++ backbones/backbone_registry.py
@@ -6,6 +6,7 @@ def register_backbone(backbone_class, name=None):
 
     if name is None:
         name = backbone_class.__name__
+        name = name.lower()
     
     if name not in backbone_registry_dict:
         backbone_registry_dict[name] = backbone_class
