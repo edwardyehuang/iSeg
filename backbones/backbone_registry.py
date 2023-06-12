15e73a8be904722b81823704f5deb35e73b7e5a8
edwardyehuang <edward.ye.huang@qq.com>
2023-06-12 23:51:24 +0800

    allow reg multiple backbones

--- backbones/backbone_registry.py
+++ backbones/backbone_registry.py
@@ -7,6 +7,13 @@ def register_backbone(backbone_class, name=None):
     if name is None:
         name = backbone_class.__name__
         name = name.lower()
-    
-    if name not in backbone_registry_dict:
-        backbone_registry_dict[name] = backbone_class
+
+    if isinstance(name, tuple):
+        name = list(name)
+
+    if not isinstance(name, list):
+        name = [name]
+
+    for n in name:
+        if n not in backbone_registry_dict:
+            backbone_registry_dict[n] = backbone_class
