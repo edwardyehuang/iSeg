23eff9eea10a2ed5e084e575548a4968327e0e6f
edwardyehuang <edward.ye.huang@qq.com>
2023-10-23 00:50:32 +0800

    add keras_wrapper

--- layers/__init__.py
+++ layers/__init__.py
@@ -8,17 +8,6 @@ from distutils.version import LooseVersion
 
 import tensorflow as tf
 
-from iseg.layers.normalizations import normalization, BATCH_NORM, SYNC_BATCH_NORM, GROUP_NROM
-
-if LooseVersion(tf.version.VERSION) < LooseVersion("2.11.0"):
-    from iseg.layers.syncbn import SyncBatchNormalization
-elif LooseVersion(tf.version.VERSION) < LooseVersion("2.14.0"):
-    SyncBatchNormalization = tf.keras.layers.experimental.SyncBatchNormalization
-else:
-    SyncBatchNormalization = functools.partial(
-        tf.keras.layers.BatchNormalization, 
-        synchronized=True
-    )
-
+from iseg.layers.normalizations import normalization, BATCH_NORM, SYNC_BATCH_NORM, GROUP_NROM, SyncBatchNormalization
 from iseg.layers.dense_ext import DenseExt
 from iseg.layers.model_builder import ConvBnRelu
