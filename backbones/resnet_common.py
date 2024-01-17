b44b49576858a7eb2fd140068032f6ae23681d25
edwardyehuang <edward.ye.huang@qq.com>
2024-01-18 04:26:52 +0800

    typo fixed

--- backbones/resnet_common.py
+++ backbones/resnet_common.py
@@ -283,9 +283,9 @@ class ResNet(tf.keras.Model):
 
         if self.replace_7x7_conv:
             stems = [
-                [self.conv1_1_conv, self.conv1_1_bn, ]
-                [self.conv1_2_conv, self.conv1_2_bn, ] 
-                [self.conv1_3_conv, self.conv1_3_bn, ]
+                [self.conv1_1_conv, self.conv1_1_bn, ],
+                [self.conv1_2_conv, self.conv1_2_bn, ], 
+                [self.conv1_3_conv, self.conv1_3_bn, ],
             ]
         else:
             stems = [[self.conv1_conv, self.conv1_bn]]
