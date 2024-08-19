f8fd0035046bb7a536534e3c0f17f7490ec08232
edwardyehuang <edward.ye.huang@qq.com>
2024-08-19 23:55:17 +0800

    rollback meaningless commits

--- backbones/resnet_blocks.py
+++ backbones/resnet_blocks.py
@@ -199,7 +199,7 @@ class BlockType2(Keras3_Model_Wrapper):
         x = self.conv3_conv(x)
         x = self.conv3_bn(x, training=training)
 
-        x = tf.add(tf.cast(shortcut, x.dtype), x)
+        x = tf.add(shortcut, x)
         x = tf.nn.relu(x)
 
         return x
