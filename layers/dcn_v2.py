aa2577b172b797147d438e41aa586f49896cfdc7
edwardyehuang <edward.ye.huang@qq.com>
2024-10-12 23:11:58 +0800

    set static shape

--- layers/dcn_v2.py
+++ layers/dcn_v2.py
@@ -154,6 +154,7 @@ class DCNv2(Keras3_Layer_Wrapper):
 
         diff_grid_concat = tf.concat([diff_grid_0, diff_grid_1], axis=-1, name="diff.grid.concat") # [B, H, W, 9, 4]
         diff_grid_concat = tf.ensure_shape(diff_grid_concat, [bs, ih, iw, self.ks, 4]) # [B, H, W, 9, 4]
+        diff_grid_concat.set_shape([None, None, None, self.ks, 4])
 
         #[B, H, W, 9, 2, 2]
         delta = tf.reshape(diff_grid_concat, [bs, ih, iw, self.ks, 2, 2])
