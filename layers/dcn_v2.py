97c51371637139a113dbae68d412e1e26d8da451
edwardyehuang <edward.ye.huang@qq.com>
2024-11-07 20:51:06 +0800

    use possible faster impl

--- layers/dcn_v2.py
+++ layers/dcn_v2.py
@@ -20,6 +20,7 @@ class DCNv2(Keras3_Layer_Wrapper):
         bias_regularizer=None,
         use_custom_offset=False,
         activation=None,
+        use_naive_forward=False,
         **kwargs
     ):
         
@@ -39,6 +40,8 @@ class DCNv2(Keras3_Layer_Wrapper):
         self.use_custom_offset = use_custom_offset
 
         self.activation = keras.activations.get(activation)
+
+        self.use_naive_forward = use_naive_forward
 
 
     def build(self, input_shape):
@@ -93,8 +96,7 @@ class DCNv2(Keras3_Layer_Wrapper):
         super().build(input_shape)
 
 
-    @tf.function(jit_compile=True, autograph=False)
-    def _forward(self, x, offset):
+    def _naive_forward(self, x, offset):
 
         #x: [B, H, W, C]
         #offset: [B, H, W, ic] convx [kh, kw, ic, 3 * groups * kh * kw] ---> [B, H, W, 3 * groups * kh * kw]
@@ -210,6 +212,125 @@ class DCNv2(Keras3_Layer_Wrapper):
         output = self.activation(output)
 
         return output
+    
+
+    @tf.function(jit_compile=True, autograph=False)
+    def _forward(self, x, offset):
+
+        #x: [B, H, W, C]
+        #offset: [B, H, W, ic] convx [kh, kw, ic, 3 * groups * kh * kw] ---> [B, H, W, 3 * groups * kh * kw]
+        offset = tf.nn.conv2d(offset, self.offset_kernel, strides=self.stride, padding="SAME")
+        offset = tf.add(offset, self.offset_bias, name="offset.add.bias")
+        bs, ih, iw, ic = get_tensor_shape(x)
+
+        ih1 = tf.add(ih, 1, name="ih.add.1")
+        iw1 = tf.add(iw, 1, name="iw.add.1")
+
+        ih1_iw1 = tf.stack([ih1, iw1], axis=0)
+
+        #[B, H, W, 18], [B, H, W, 9]
+        oyox, mask = offset[..., :2 * self.ks], offset[..., 2 * self.ks:]
+
+        oyox = tf.reshape(oyox, [bs, ih, iw, self.ks, 2])
+
+        mask = tf.nn.sigmoid(mask)
+        #[H, W, 2]
+        grid_yx = tf.stack(tf.meshgrid(tf.range(iw), tf.range(ih))[::-1], axis=-1)
+        #[1, H, W, 9, 2]
+        grid_yx = tf.reshape(grid_yx, [1, ih, iw, 1, 2]) 
+        grid_yx = tf.add(grid_yx, self.phw, name="grid_yx.add.phw")
+        grid_yx = tf.add(grid_yx, self.patch_yx, name="grid_yx.add.patch_yx")
+
+        #[B, H, W, 9, 2]
+        grid_yx = tf.add(tf.cast(grid_yx, oyox.dtype), oyox, name="grid.add.oyox")
+
+        grid_iy0ix0 = tf.floor(grid_yx)
+        grid_iy1ix1 = tf.clip_by_value(
+            tf.add(grid_iy0ix0, tf.ones_like(grid_iy0ix0), name="grid_iy0ix0.add.ones"), 
+            0, 
+            tf.cast(ih1_iw1, dtype=grid_iy0ix0.dtype)
+        ) # 
+
+        #[B, H, W, 9, 1] * 2
+        grid_iy1, grid_ix1 = tf.split(grid_iy1ix1, 2, axis=4)
+        grid_iy0ix0 = tf.clip_by_value(grid_iy0ix0, 0, tf.cast(ih1_iw1, dtype=grid_iy0ix0.dtype))
+        grid_iy0, grid_ix0 = tf.split(grid_iy0ix0, 2, axis=4)
+
+        grid_yx = tf.clip_by_value(grid_yx, 0, tf.cast(ih1_iw1, dtype=grid_yx.dtype))
+
+        #[B, H, W, 9, 4, 1]
+        batch_index = tf.tile(tf.reshape(tf.range(bs), [bs, 1, 1, 1, 1, 1]), [1, ih, iw, self.ks, 4, 1])
+
+        
+        grid = tf.concat(
+            [grid_iy1ix1, grid_iy1, grid_ix0, grid_iy0, grid_ix1, grid_iy0ix0], 
+            axis=-1,
+            name="all.grid.concat0",
+        ) # [B, H, W, 9, 8]
+        
+        #[B, H, W, 9, 4, 2]
+        grid = tf.reshape(grid, [bs, ih, iw, self.ks, 4, 2])
+        
+
+        grid = tf.concat([batch_index, tf.cast(grid, tf.int32)], axis=-1, name="grid.concat.batch.index") # [B, H, W, 9, 4, 3]
+
+        diff_grid_0 = grid_yx - grid_iy0ix0 # [B, H, W, 9, 2]
+        diff_grid_1 = grid_iy1ix1 - grid_yx # [B, H, W, 9, 2]
+
+        diff_grid_0_0, diff_grid_0_1 = tf.unstack(diff_grid_0, 2, axis=-1) # [B, H, W, 9]
+        diff_grid_1_0, diff_grid_1_1 = tf.unstack(diff_grid_1, 2, axis=-1) # [B, H, W, 9]
+
+        diff_grid_concat = tf.stack([diff_grid_0_0, diff_grid_0_1, diff_grid_1_0, diff_grid_1_1], axis=-1, name="diff.grid.stack") # [B, H, W, 9, 4]
+
+        #[B, H, W, 9, 2, 2]
+        delta = tf.reshape(diff_grid_concat, [bs, ih, iw, self.ks, 2, 2]) # [B, H, W, 9, 2, 2]
+
+        w0, w1 = tf.unstack(delta, 2, axis=-1) # [B, H, W, 9, 2], [B, H, W, 9, 2]
+        w0 = tf.expand_dims(w0, axis=-1) # [B, H, W, 9, 2, 1]
+        w1 = tf.expand_dims(w1, axis=-2) # [B, H, W, 9, 1, 2]
+
+        w = w0 * w1 # [B, H, W, 9, 2, 2]
+
+        #[B, H+2, W+2, C]
+        x = tf.pad(x, [[0, 0], [int(self.ph), int(self.ph)], [int(self.pw), int(self.pw)], [0, 0]])
+        
+        mask = tf.expand_dims(mask, axis=-1) # [B, H, W, 9, 1]
+        mask = tf.unstack(mask, self.ks, axis=3) # [B, H, W, 1] * 9
+
+        w = tf.reshape(w, [bs, ih, iw, self.ks, 4]) # [B, H, W, 9, 4]
+        w = tf.unstack(w, self.ks, axis=3) # [B, H, W, 4] * 9
+                       
+        map_bilinear = [None] * self.ks
+
+        grid = tf.transpose(grid, [3, 0, 1, 2, 4, 5]) # [9, B, H, W, 4, 3]
+
+        for i in range(self.ks):
+
+            _map_sample = tf.gather_nd(x, grid[i]) # [B, H, W, 4, C]
+            _w = w[i] # [B, H, W, 4]
+            _w = tf.expand_dims(_w, axis=-2) # [B, H, W, 1, 4]
+            _mask = mask[i] # [B, H, W, 1]
+
+            _map_bilinear = tf.matmul(_w, _map_sample) # [B, H, W, 1, C]
+            _map_bilinear = tf.squeeze(_map_bilinear, axis=-2)
+
+            _map_bilinear = tf.multiply(_map_bilinear, _mask) # [B, H, W, C]
+
+            map_bilinear[i] = _map_bilinear
+
+        map_bilinear = tf.stack(map_bilinear, axis=-2) # [B, H, W, 9, C]
+
+        #[B, H, W, 9*C]
+        map_all = tf.reshape(map_bilinear, [bs, ih, iw, -1])
+        #[B, H, W, OC]
+        output = tf.nn.conv2d(map_all, tf.reshape(self.kernel, [1, 1, -1, self.filters]), strides=self.stride, padding='SAME')
+
+        if self.use_bias:
+            output = tf.add(output, tf.cast(self.bias, output.dtype), name="output.add.bias")
+
+        output = self.activation(output)
+
+        return output
         
         
     def call(self, inputs, training=None):
@@ -219,8 +340,10 @@ class DCNv2(Keras3_Layer_Wrapper):
         else:
             x = offset = inputs
 
-        return self._forward(x, offset)
-        
+        if self.use_naive_forward:
+            return self._naive_forward(x, offset)
+        else:
+            return self._forward(x, offset)
         
         
     def compute_output_shape(self, input_shape):
